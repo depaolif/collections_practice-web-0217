fe4289333569b9bf09bdac3cf1c8f1b2e76c23a0
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-02-07 16:22:01 -0500

    tested

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+def sort_array_asc(array)
+	array.sort
+end
 
+def sort_array_desc(array)
+	array.sort.reverse
+end
+
+def sort_array_char_count(array)
+	array.sort do |a,b|
+		a.length <=> b.length
+	end
+end
+
+def swap_elements(array)
+	array.insert(1,array.delete_at(2))
+end
+
+def reverse_array(array)
+	array.reverse
+end
+
+def kesha_maker(array)
+	new_array = []
+	array.each do |item|
+		new_array << item[0..1] + "$" + item[3..item.length]
+	end
+	new_array
+end
+
+def find_a(array)
+	array.select do |i|
+		i.start_with?('a')
+	end
+end
+
+def sum_array(array)
+	array.reduce do |a,b|
+		a + b
+	end
+end
+
+def add_s(array)
+	array.each_with_index.map { |e, i| 
+		if i != 1
+			e + "s"
+		else
+			e
+		end
+	}
+end
